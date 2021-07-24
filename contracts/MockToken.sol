56e097e71afb9981b449b48736dd0b621c5d86b3
quardz <adal@quardz.com>
2021-07-24 18:37:44 +0530

    fixed font burn in staking

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MockToken is ERC20 {
+
+contract MockToken is ERC20, ERC20Burnable {
     constructor(string memory name,string memory symbol, uint8 decimals) ERC20(name, symbol) {
         _mint(msg.sender, 2000000 * 10 ** decimals);
     }
 }
+
